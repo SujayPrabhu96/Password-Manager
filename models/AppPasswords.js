8d8dd209e3f8d4a1e5f79541a46d4859d48d5d30
SujayPrabhu96 <prabhusujay@gmail.com>
2020-07-06 22:54:33 +0530

    Updated Models

---
+++ models/AppPasswords.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+module.exports = db.sequelize.define(
+    'app_pwds',
+    {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: Sequelize.INTEGER
+        },
+        app: {
+            type: Sequelize.STRING
+        },
+        password: {
+            type: Sequelize.STRING
+        }
+    },
+    {
+        timestamps: false
+    }
+)
