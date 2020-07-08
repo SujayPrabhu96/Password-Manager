a473a0fb6c30562a429a57f62b4a6216614142fd
SujayPrabhu96 <prabhusujay@gmail.com>
2020-07-08 22:51:49 +0530

    Store hashed password

--- Controllers/RegisterController.js
+++ Controllers/RegisterController.js
@@ -1,4 +1,13 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+const generateHash = (password) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, (err, hash) => resolve(hash));
+        });
+    });
+};
 
 const displayForm = (req, res) => {
     res.render('users/register', {
@@ -6,9 +15,10 @@ const displayForm = (req, res) => {
     });
 };
 
-const registerUser = (req, res) => {
+const registerUser = async (req, res) => {
     const errors = [];
-    const { name , email , password, confirm_pwd } = req.body;
+    let { name , email , password, confirm_pwd } = req.body;
+    password = await generateHash(password);
     User.create({ name, email, password})
     .then(user => res.render('users/register', {title: 'Register'})) //need to create login route
     .catch(error => {
