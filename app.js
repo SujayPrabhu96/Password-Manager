571da99b90d92a837f7760e67e30b37470b48cde
SujayPrabhu96 <prabhusujay@gmail.com>
2020-07-10 22:38:56 +0530

    Login with passport

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const session    = require('express-session');
 const app = express();
 const { port } = require('./config');
 const userRouter = require('./routes/users');
+require('./passport')(passport);
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//for passport
+app.use(session({ secret: 'secret',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session());
 
 //render landing page
 app.get('/', (req, res) => res.render('./index'));
---
+++ passport.js
@@ -0,0 +1,41 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const User = require('./models/User');
+
+module.exports = (passport) => {
+        passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+            User.findOne({
+                where: {
+                    email: email
+                }
+            })
+            .then(user => {
+                if(!user){
+                    return done(null, false, {message: 'Email is not registered'})
+                }
+                bcrypt.compare(password, user.password, (err, isMatch) => {
+                    if(err){
+                        return done(err);
+                    }
+                    if(isMatch){
+                        return done(null, user);
+                    }
+                    return done(null, false, {message: 'Incorrect Password'});
+                })
+            })
+            .catch(error => {
+                return done(null, false, {message: 'Something Went Wrong with SignIn'})
+            });
+        })
+    );
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+      
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => {
+          done(err, user);
+        });
+    });
+}; 
