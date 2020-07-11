8919f0b3442c7b6a07ac450faafca0c72327d98c
SujayPrabhu96 <prabhusujay@gmail.com>
2020-07-11 20:48:53 +0530

    Changes based on review of Register

--- helpers/validate.js
+++ helpers/validate.js
@@ -2,8 +2,8 @@ const { check } = require('express-validator');
 
 const validate_reg = () => {
     return [ 
-        check('name').notEmpty().withMessage('Name field should not be Empty'),
-        check('email').notEmpty().withMessage('Email feild should not be Empty'),
+        check('name').trim().notEmpty().withMessage('Name field should not be Empty'),
+        check('email').trim().notEmpty().withMessage('Email feild should not be Empty'),
         check('email').isEmail().withMessage('Email is not in proper format'),
         check('password').isLength({ min: 6}).withMessage('Password must be of atleast 6 characters'),
         check('confirm_pwd').isLength({ min: 6}).withMessage('Password must be of atleast 6 characters')         
