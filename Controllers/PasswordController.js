4bbc0d06b1955387e987842a9bbc5349f6c6ab06
SujayPrabhu96 <prabhusujay@gmail.com>
2020-07-16 23:45:24 +0530

    Save App-Passwords

--- Controllers/PasswordController.js
+++ Controllers/PasswordController.js
@@ -30,5 +30,50 @@ const displayAddForm = (req, res) => {
     });
 };
 
+const userAppExists = async (user_id, app) => {
+    try{
+        return await AppPassword.count({ where: {'user_id': user_id, 'app': app}});
+    } catch(error){
+        throw new Error(error);
+    }
+};
+
+const updatePassword = async(user_id, params) => {
+    try{
+        let { app, username, password } = params;
+        return await AppPassword.update(
+            { app, username, password },
+            { where: { 'user_id': user_id, 'app': app } }
+        );
+    } catch(error) {
+        throw new Error(errro);
+    }
+};
+
+const savePassword = async(req, res) => {
+    const user_id = req.user.id;
+    const date = new Date().toISOString().slice(0, 10);
+    let { app, username, password } = req.body;
+    try{
+        let count = await userAppExists(user_id, app); //check if entry is present for app with logged in user
+        if(count){
+            await updatePassword(user_id, req.body);
+        } else {
+            await AppPassword.create({ user_id, date, app, username, password });
+        }
+        res.redirect('/apps/passwords');
+    } catch(error) {
+        let errors = [];
+        errors.push({msg: error});
+        res.render('apps/add_password', {
+            title: 'Add Application-Password',
+            isLoggedIn: true,
+            errors
+        });
+    }
+    
+}
+
 module.exports.listPasswords = listPasswords;
 module.exports.displayAddForm = displayAddForm;
+module.exports.savePassword = savePassword;
