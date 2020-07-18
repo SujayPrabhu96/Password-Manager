6c0ef52aa9d6efe48b5ce83cfdef0e9d2849fd57
SujayPrabhu96 <prabhusujay@gmail.com>
2020-07-18 19:47:34 +0530

    Validation to add form

--- routes/password.js
+++ routes/password.js
@@ -2,11 +2,26 @@ const express = require('express');
 const router = express.Router();
 const { listPasswords, displayAddForm, savePassword } = require('../Controllers/PasswordController');
 const { isLoggedIn } = require('../auth');
+const { validationResult } = require('express-validator');
+const Validation = require('../helpers/validate');
 
 router.get('/passwords', isLoggedIn, listPasswords);
 
 router.get('/add-password', isLoggedIn, displayAddForm);
 
-router.post('/save-password', isLoggedIn, savePassword);
+router.post('/add-password', [Validation.validate_pwd_form()], isLoggedIn, (req, res) => {
+    const errors = validationResult(req).errors;
+    const password = req.body.password;
+    const confirm_password = req.body.confirm_password;
+    if(password != confirm_password){
+        errors.push({msg: 'Password and Confirm Password should be same'});
+        res.render('apps/add_password', {
+            title: 'Add Application-Password',
+            isLoggedIn: true,
+            errors
+        });
+    }
+    savePassword(req, res);
+});
 
 module.exports = router;
