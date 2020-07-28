724ea76f7e38a6cf0a587f2b8149d48b69441186
SujayPrabhu96 <prabhusujay@gmail.com>
2020-07-28 19:52:29 +0530

    Updated strict equality

--- routes/password.js
+++ routes/password.js
@@ -12,7 +12,7 @@ router.get('/add-password', isLoggedIn, displayAddForm);
 router.post('/add-password', [Validation.validate_pwd_form()], isLoggedIn, (req, res) => {
     const errors = validationResult(req).errors;
     const { password, confirm_password } = req.body;
-    if(password != confirm_password){
+    if(password !== confirm_password){
         errors.push({msg: 'Password and Confirm Password should be same'});
         res.render('apps/add_password', {
             title: 'Add Application-Password',
